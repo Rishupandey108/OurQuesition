4fd4ded0c9e80ca4b3d1a2c594863c9c1f0b53b9
Rishu <rishupandey9546@gmail.com>
2026-07-07 08:47:31 +0530

    Time: 41 ms (31.34%), Space: 54.9 MB (56.72%) - LeetHub

--- 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.java
+++ 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.java
@@ -1,18 +1,19 @@
 class Solution {
     public int minOperations(int[] nums) {
         int result =0;
+        int n = nums.length;
 
         while(true)
         {
             boolean flag = false;
-            for(int i=0;i<nums.length;i++){
+            for(int i=0;i<n;i++){
                 if(nums[i]%2!=0){
                     nums[i]--;
                     result++;
                 }
             }
 
-            for(int i=0;i<nums.length;i++){
+            for(int i=0;i<n;i++){
                 
                  nums[i]/=2;
 
