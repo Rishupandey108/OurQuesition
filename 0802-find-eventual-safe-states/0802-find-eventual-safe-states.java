0a9308da64fcf1c8715f21b9520ca48d70da6329
Rishu <rishupandey9546@gmail.com>
2026-08-10 21:54:10 +0530

    Time: 11 ms (63.81%), Space: 64.3 MB (15.58%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,35 +1,59 @@
 class Solution {
+
+    public static class Edges{
+        int src;
+        int dest;
+        Edges(int s,int d){
+            this.src=s;
+            this.dest=d;
+        }
+    }
+
+    public static boolean dfs(int curr,ArrayList<Edges>graph[],boolean[]vis,boolean[]rec){
+        vis[curr] = true;
+        rec[curr] = true;
+
+        for(int i=0;i<graph[curr].size();i++){
+            Edges e = graph[curr].get(i);
+
+            if(rec[e.dest]){
+                return true;
+            }else if(!vis[e.dest]){
+                if(dfs(e.dest,graph,vis,rec)){
+                    return true;
+                }
+            }
+        }
+
+        rec[curr] = false;
+        return false;
+    }
+
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> result = new ArrayList<>();
 
-        int n = graph.length;
+        ArrayList<Edges>Graph[] = new ArrayList[graph.length];
 
-        int[] check = new int[n];
+        for(int i=0;i<graph.length;i++){
+            Graph[i]=new ArrayList<Edges>();
+        }
 
-
-        for(int i =0;i<n;i++){
-
-            if(dfs(i,graph,check)){
-                result.add(i);
+        for(int i=0;i<graph.length;i++){
+            for(int a:graph[i]){
+                Graph[i].add(new Edges(i,a));
             }
+        }
+
+        boolean vis[] = new boolean[graph.length];
+        boolean rec[] = new boolean[graph.length];
+
+        for(int i=0;i<graph.length;i++){
+
+                if(!dfs(i,Graph,vis,rec)){
+                        result.add(i);
+                }
         }
 
         return result;
-    }
-
-    public static boolean dfs(int curr, int[][]graph,int[]vis){
-
-        if(vis[curr]>0) return vis[curr]==2;
-
-        vis[curr] = 1;
-
-        for(int a:graph[curr]){
-            if(vis[curr]==1 &&!dfs(a,graph,vis)){
-                return false;
-            }
-        }
-
-        vis[curr] = 2;
-        return true;
     }
 }
