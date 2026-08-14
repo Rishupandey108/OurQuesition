87e61e322a582b577dc4570dc3563f9ae6a97b55
Rishu <rishupandey9546@gmail.com>
2026-08-14 21:22:33 +0530

    Time: 1 ms (100%), Space: 46.3 MB (89.47%) - LeetHub

---
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int semiOrderedPermutation(int[] nums) {
+         int n = nums.length;
+         int result =0;
+
+         int min=1,max=nums.length;
+
+        
+        for(int i=0;i<=nums.length/2-1;i++){
+            if(nums[i]==1){
+                min = i;
+               
+            }else if(nums[i]==nums.length){
+                    max =i;
+                     
+            }
+        }
+
+        for(int i=nums.length/2;i<nums.length;i++){
+            if(nums[i]==n){
+                max = i;
+                
+            }else if(nums[i]==1){
+                min = i;
+                
+            }
+        }
+
+
+        result += Math.abs(0-min);
+        result += Math.abs(n-max-1);
+
+        if(max<min){
+            result-=1;
+        }
+
+        
+         return result; 
+
+          
+    }
+}
