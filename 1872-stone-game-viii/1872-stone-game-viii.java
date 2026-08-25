bc70fbff07c0176754af115e89758382aee541b9
Rishu <rishupandey9546@gmail.com>
2026-08-25 14:00:31 +0530

    Time: 3 ms (100%), Space: 83.3 MB (87.18%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+
+        int n = stones.length;
+        
+        for(int i=1;i<n;i++){
+            stones[i] = stones[i]+stones[i-1];
+        }
+
+
+        int result = stones[n-1];
+
+        for(int i=n-2;i>0;i--){
+             result = Math.max(result,stones[i]-result);
+        }
+
+        return result;
+    }
+}
