adfd40c6f95cef14add6e2da3d4cc8cdc95d407f
Rishu <rishupandey9546@gmail.com>
2026-08-27 12:54:30 +0530

    Time: 8 ms (62.63%), Space: 47.1 MB (21.72%) - LeetHub

--- 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.java
+++ 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.java
@@ -7,11 +7,12 @@ class Solution {
         for(int i=0;i<size;i++){
 
                 HashSet<Integer> hst  =new HashSet<>();
+
             for(int j=i;j<size;j++){
 
                 hst.add(nums.get(j));
 
-                result += hst.size()*hst.size();
+                result += Math.pow(hst.size(),2);
 
             }
         }
