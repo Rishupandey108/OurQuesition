232fda71df5307fb0f7a876c9a55943ab2da4937
Rishu <rishupandey9546@gmail.com>
2026-08-29 20:19:08 +0530

    Time: 11 ms (31%), Space: 43.9 MB (78.72%) - LeetHub

---
+++ 0481-magical-string/0481-magical-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int magicalString(int n) {
+        
+        if(n==0) return 0;
+
+        if(n<=3) return 1;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("122");
+        int index = 2;
+        char ch ='1';
+
+        while(sb.length()!=n){
+
+                 int val =  sb.charAt(index)-'0';
+
+
+                 for(int i=0;i<val;i++){
+
+                    sb.append(ch);
+
+                    if(sb.length()==n){
+                        break;
+                    }
+
+                 }
+
+                 if(ch=='1'){
+                    ch='2';
+                 }else{
+                    ch='1';
+                 }
+                 index +=1;
+        }
+
+        int result =0;
+
+        for(int i=0;i<sb.length();i++){
+            if(sb.charAt(i)=='1'){
+                result++;
+            }
+        }
+
+        return result;
+
+    }
+}
