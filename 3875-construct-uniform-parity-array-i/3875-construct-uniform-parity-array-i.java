f4fce8b636a111101d9a9fbbf010d62f3b75ff7e
Rishu <rishupandey9546@gmail.com>
2026-09-02 14:19:17 +0530

    Time: 0 ms (100%), Space: 45.3 MB (35.83%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        
+        // for(int i=0;i<nums1.length-1;i++){
+
+        //         if(nums1[i]%2==0 && nums[i+1]%2==0){
+        //             return 
+        //         }
+        // // }
+
+        return true;
+    }
+}
