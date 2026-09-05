cd0af55f8950fc24057d5058f3ddf345aa975b7c
Rishu <rishupandey9546@gmail.com>
2026-09-05 11:34:32 +0530

    Time: 3 ms (83.31%), Space: 56.3 MB (28.16%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        
+        Arrays.sort(score,(a,b)->{
+            return Integer.compare(b[k],a[k]);
+        });
+
+        return score;
+    }
+}
