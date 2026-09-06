dbbac5fd7699af6640a55de9fa9fa0a7e86413c6
Rishu <rishupandey9546@gmail.com>
2026-09-06 08:32:47 +0530

    Time: 20 ms (53.25%), Space: 54.2 MB (62.97%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,23 +1,34 @@
 class Solution {
-    private int distinct(int i,int j,String s,String t,int[][]dp){
-        if(j==0) return 1;
-        if(i==0) return 0;
+    // private int distinct(int i,int j,String s,String t,int[][]dp){
+    //     if(j==0) return 1;
+    //     if(i==0) return 0;
         
-        if(dp[i][j]!=-1) return dp[i][j];
+    //     if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s.charAt(i-1)==t.charAt(j-1)){
-            return dp[i][j] = distinct(i-1,j-1,s,t,dp) + distinct(i-1,j,s,t,dp);
-        }
+    //     if(s.charAt(i-1)==t.charAt(j-1)){
+    //         return dp[i][j] = distinct(i-1,j-1,s,t,dp) + distinct(i-1,j,s,t,dp);
+    //     }
 
-        return dp[i][j] = distinct(i-1,j,s,t,dp);
-    }
+    //     return dp[i][j] = distinct(i-1,j,s,t,dp);
+    // }
     public int numDistinct(String s, String t) {
         int [][] dp = new int[s.length()+1][t.length()+1];
+        int n = s.length();
+        int m = t.length();
 
-        for(int arr[]:dp){
-            Arrays.fill(arr,-1);
+         
+        for(int i=0;i<=n;i++) dp[i][0] = 1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
 
-        return distinct(s.length(),t.length(),s,t,dp);
+        return dp[n][m];
     }
 }
