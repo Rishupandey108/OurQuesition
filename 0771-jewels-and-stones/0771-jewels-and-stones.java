3054f276b5359f480ad77339283c4371f8803e5b
Rishu <rishupandey9546@gmail.com>
2026-09-13 10:44:09 +0530

    Time: 0 ms (100%), Space: 43.2 MB (35.8%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        
+        int result=0;
+
+        for(int i=0;i<stones.length();i++){
+            if(jewels.indexOf(stones.charAt(i))>=0){
+                result+=1;
+            }
+        }
+
+        return result;
+    }
+}
