aec9f3f0c90709d0eb65c59462e6d5d5eabf5d10
Rishu <rishupandey9546@gmail.com>
2026-09-20 22:21:15 +0530

    Time: 0 ms (100%), Space: 43.4 MB (54.75%) - LeetHub

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
@@ -1,23 +1,13 @@
 class Solution {
     public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
         
-        return FindColor(coordinate1).equals(FindColor(coordinate2));
+        return FindColor(coordinate1)==FindColor(coordinate2);
     }
 
 
-    private  String FindColor(String s){
+    private  boolean FindColor(String s){
 
-        int alpha = s.charAt(0)-'a';
-        int digit = s.charAt(1)-'0';
-
-        if(alpha%2==1 && digit%2==1){
-            return "White";
-        }
-        if(alpha%2==0 && digit%2==0){
-            return "White";
-        }
-
-        return "Black";
-
+        return (s.charAt(0)-'a')%2==(s.charAt(1)-'0')%2;
+        
     }
 }
