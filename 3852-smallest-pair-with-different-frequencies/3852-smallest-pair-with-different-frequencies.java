093583f9743b81e55b87517dc34310c472083d44
Rishu <rishupandey9546@gmail.com>
2026-09-26 11:22:41 +0530

    Time: 6 ms (55.14%), Space: 46.8 MB (61.99%) - LeetHub

--- 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
@@ -1,7 +1,5 @@
 class Solution {
     public int[] minDistinctFreqPair(int[] nums) {
-        
-
         HashMap<Integer,Integer> hsmp = new HashMap<>();
 
         for(int a:nums){
@@ -21,21 +19,14 @@ class Solution {
            Integer.compare(a[0],b[0])
         );
 
-        int [] res = new int[2];
-
-        for( i=0;i<arr.length;i++){
+        for(i=0;i<arr.length;i++){
 
             for(int j=i+1;j<arr.length;j++){
 
                 if(arr[i][1]!=arr[j][1]){
-                    res[0] = arr[i][0];
-                    res[1] = arr[j][0];
-                    // Arrays.sort(res);
-                    return res;
+                    return new int[]{arr[i][0],arr[j][0]};
                 }
-            }
-
-            
+            }  
         }
 
         return new int[] {-1,-1};
