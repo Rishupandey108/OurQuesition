2c832e39c726aea26efafbee90e8cca18cbe806b
Rishu <rishupandey9546@gmail.com>
2026-09-26 11:20:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] minDistinctFreqPair(int[] nums) {
+        
+
+        HashMap<Integer,Integer> hsmp = new HashMap<>();
+
+        for(int a:nums){
+            hsmp.put(a,hsmp.getOrDefault(a,0)+1);
+        }
+
+        int arr[][] = new int[hsmp.size()][2];
+        int i=0;
+
+        for(int a:hsmp.keySet()){
+            arr[i][0] = a;
+            arr[i][1] = hsmp.get(a);
+            i++;
+        }
+
+        Arrays.sort(arr,(a,b) -> {
+            if(a[1]!=b[1]){
+                return Integer.compare(a[1],b[1]);
+            }
+            return Integer.compare(a[0],b[0]);
+        });
+
+        int [] res = new int[2];
+
+        for( i=0;i<arr.length;i++){
+
+            for(int j=i+1;j<arr.length;j++){
+
+                if(arr[i][1]!=arr[j][1]){
+                    res[0] = arr[i][0];
+                    res[1] = arr[j][0];
+                    Arrays.sort(res);
+                    return res;
+                }
+            }
+
+            
+        }
+
+        return new int[] {-1,-1};
+    }
+}
